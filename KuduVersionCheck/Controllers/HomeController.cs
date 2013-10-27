46b5c5c8c4c3fd69af031b6e371c1a203875ef52
davidebbo <david.ebbo@microsoft.com>
2013-10-26 22:29:11 -0700

    Rename test sites

--- KuduVersionCheck/Controllers/HomeController.cs
+++ KuduVersionCheck/Controllers/HomeController.cs
@@ -11,12 +11,12 @@ namespace KuduVersionCheck.Controllers
     {
         public async Task<ActionResult> Index()
         {
-            string[] regions = { "WestUS", "NorthCentralUS", "EastUS", "WestEurope", "NorthEurope", "EastAsia" };
+            string[] regions = { "bay-001", "ch1-001", "blu-001", "db3-001", "am2-001", "hk1-001" };
 
             var client = new HttpClient();
             var commitIds = await Task.WhenAll(
                 regions.Select(async region => {
-                    var response = await client.GetAsync(String.Format("http://Kudu{0}.azurewebsites.net/", region));
+                    var response = await client.GetAsync(String.Format("http://kudu-{0}.azurewebsites.net/", region));
                     return await response.Content.ReadAsStringAsync();
                 }
             ));
